0528981d3a1edbaf3582b9f44f62596130ec6ba8
BarPus <katerinalove02@mail.ru>
2021-03-04 09:03:54 +0700

    program code formating

--- geometry.cpp
+++ geometry.cpp
@@ -1,39 +1,52 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 #include <regex>
 using namespace std;
 
-int main () {
-	string Figure;                                                                //принимаем строку с фигурой 
-	getline(cin, Figure);
-	
-if((strstr(Figure.c_str(),"circle ")) || (strstr(Figure.c_str(),"circle("))) {   //проверяем правильности фигуры "окружность"
-                                                                           
-	cmatch result;
-	regex regular ("([\\w\\s]+)"
-					"(\\()"  
-					"([\\s]*)"    
-					"([\\d]*)"
-					"([\\s]*)"                                                     //проверяем входящую строку
-					"([\\d]*)"
-					"([\\s]*)"  
-					"(\\,)"
-					"([\\s]*)"  
-					"([0-9]*[.]?[0-9]+)"
-					"([\\s]*)"  
-					"(\\))"  );  
-	
-	if (regex_match(Figure.c_str(), result, regular)) {
-		cout <<"true" <<endl;
-//		for (int i=0; i<result.size(); i++) {
-//			cout <<result[i++] <<endl;         -вывод каждого участка
-//			}	
-	}
+void Circle(string Figure)
+{
+    if ((strstr(Figure.c_str(), "circle "))
+        || (strstr(
+                Figure.c_str(),
+                "circle("))) { //проверяем правильности фигуры "окружность"
 
-//string R=result.str(10);
-//cout <<R; 
-	
+        cmatch result;
+        regex regular(
+                "([\\w\\s]+)"
+                "(\\()"
+                "([\\s]*)"
+                "([0-9]+)"
+                "([\\s]*)" //проверяем входящую строку
+                "([0-9]+)"
+                "([\\s]*)"
+                "(\\,)"
+                "([\\s]*)"
+                "([0-9]*[.]?[0-9]+)"
+                "([\\s]*)"
+                "(\\))");
+
+        if (regex_match(Figure.c_str(), result, regular)) {
+            cout << "true" << endl;
+
+            //		for (int i=0; i<result.size(); i++) {
+            //			cout <<result[i++] <<endl;         -вывод каждого
+            //участка
+            //			}
+        }
+
+        // string R=result.str(10);
+        // cout <<R;
+
+        else
+            cout << "error" << endl;
+    } else
+        cout << "error" << endl;
 }
-else cout <<"error" <<endl;
+
+int main()
+{
+    string Figure; //принимаем строку с фигурой
+    getline(cin, Figure);
+    Circle(Figure);
+    return 0;
 }
-	
