212355811f73a90d925395b562b81e69100ac6f9
BarPus <katerinalove02@mail.ru>
2021-03-17 15:17:18 +0700

    modified code

--- geometry.cpp
+++ geometry.cpp
@@ -60,6 +60,7 @@ float Perimeter(float R)
     float P, pi = 3.14;
     P = 2 * pi * R;
     cout << "Perimeter =" << P << endl;
+    return P;
 }
 
 float Area(float R)
@@ -67,6 +68,7 @@ float Area(float R)
     float S, pi = 3.14;
     S = pi * R * R;
     cout << "Area =" << S << endl;
+    return S;
 }
 
 int main()
