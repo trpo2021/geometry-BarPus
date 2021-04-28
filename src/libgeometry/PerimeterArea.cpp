f59c110519bd6af3a4d89d5e9bfb8c4a281f41cc
BarPus <katerinalove02@mail.ru>
2021-04-28 16:32:47 +0700

    Fix code

--- src/libgeometry/PerimeterArea.cpp
+++ src/libgeometry/PerimeterArea.cpp
@@ -1,21 +1,20 @@
 #include "PerimeterArea.h"
 #include <string>
 #include <iostream>
+#include <math.h>
 #include <regex>
 using namespace std;
 
 float Perimeter(float R)
 {
-    float P, pi = 3.14;
-    P = 2 * pi * R;
-    cout << "Perimeter =" << P << endl;
+    float P;
+    P = 2 * M_PI * R;
     return P;
 }
 
 float Area(float R)
 {
-    float S, pi = 3.14;
-    S = pi * R * R;
-    cout << "Area =" << S << endl;
+    float S;
+    S = M_PI * R * R;
     return S;
 }
